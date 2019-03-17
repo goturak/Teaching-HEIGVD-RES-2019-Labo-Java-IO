fbde7e9471e8f4537fb0b26a4bf2bf53270a8a14
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-03-17 15:11:46 +0100

    Application.java done
    everything is green :)

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -2,15 +2,15 @@ package ch.heigvd.res.labio.impl;
 
 import ch.heigvd.res.labio.impl.explorers.DFSFileExplorer;
 import ch.heigvd.res.labio.impl.transformers.CompleteFileTransformer;
+import ch.heigvd.res.labio.impl.transformers.FileTransformer;
+import ch.heigvd.res.labio.impl.transformers.NoOpFileTransformer;
 import ch.heigvd.res.labio.interfaces.IApplication;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +90,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote,"quote-"+i+".utf8");
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +124,18 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String path=WORKSPACE_DIRECTORY;
+    for(String s : quote.getTags()){
+     path+=("/"+s);
+    }
+    new File(path).mkdirs();
+    path+=("/"+filename);
+
+    File newQuote = new File(path);
+    OutputStreamWriter writer = new OutputStreamWriter( new FileOutputStream(newQuote), "UTF-8" );
+    writer.write(quote.getQuote());
+    writer.close();
+
   }
   
   /**
@@ -135,7 +147,12 @@ public class Application implements IApplication {
     explorer.explore(new File(WORKSPACE_DIRECTORY), new IFileVisitor() {
       @Override
       public void visit(File file) {
-        /*
+          try {
+              writer.write(file.getPath() + '\n');
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+          /*
          * There is a missing piece here. Notice how we use an anonymous class here. We provide the implementation
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -19,23 +21,92 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineNumber;
+  private int charIndex;
+  private char prevChar;
+  private boolean newLine;
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    lineNumber=1;
+    charIndex=0;
+    newLine=true;
+    prevChar=Character.MIN_VALUE;
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String result=new String();
+    str= str.substring(off,off+len);
+    int excessLength=0;
+
+    if(newLine){
+      result=result.concat( Integer.toString(lineNumber));
+      result=result.concat("\t");
+      excessLength+= (1 +Integer.toString(lineNumber).length());
+      lineNumber++;
+      newLine=false;
+    }
+    String[] lines= Utils.getNextLine(str);
+
+    if(!lines[0].isEmpty()){
+      result=result.concat(lines[0]);
+      if(lines[1].isEmpty()){
+        result=result.concat( Integer.toString(lineNumber));
+        result=result.concat("\t");
+        excessLength+= (1 +Integer.toString(lineNumber).length());
+        lineNumber++;
+        newLine=false;
+
+      }
+    }
+    super.write(result,0,result.length());
+    if(!lines[1].isEmpty()){
+
+      if( lines[0].isEmpty()){
+        newLine=false;
+        super.write(lines[1],0,lines[1].length());
+      }else{
+        newLine=true;
+        write(lines[1],0,lines[1].length());
+      }
+
+    }
+
+
+
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      String str= new String(cbuf);
+      write(str,off,len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    char currentChar= (char) c;
+    boolean tempNewLine= false;
+    if (prevChar == '\r') {
+      if(currentChar=='\n'){
+        tempNewLine=true;
+        newLine=false;
+      }
+    }
+    if(currentChar=='\n'||currentChar=='\r'){
+      tempNewLine=true;
+    }
+    if(newLine){
+        String lineStr=Integer.toString(lineNumber);
+        for (int i = 0; i < lineStr.length();i++) {
+          super.write(lineStr.charAt(i));
+        }
+        super.write('\t');
+        lineNumber++;
+    }
+    newLine=tempNewLine;
+    prevChar=currentChar;
+    super.write(currentChar);
+
   }
 
 }
